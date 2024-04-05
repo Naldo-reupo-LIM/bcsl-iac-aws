7370c71ae7d7e4de3336ac779b26704e8c882d36
Naldo-reupo-LIM <naldo_reupo-musayon@mckinsey.com>
2024-04-05 09:30:42 -0500

    feat: add variables to task definitions

--- modules/ecs/ecs_task_backend.tf
+++ modules/ecs/ecs_task_backend.tf
@@ -14,7 +14,7 @@ resource "aws_ecs_task_definition" "back_task" {
     "name": "backend-image",
     "networkMode": "awsvpc",
     "linuxParameters": {
-	    "initProcessEnabled": true
+      "initProcessEnabled": true
     },
     "enableExecuteCommand": true,
     "portMappings": [
@@ -22,9 +22,108 @@ resource "aws_ecs_task_definition" "back_task" {
         "containerPort": 5002,
         "hostPort": 5002
       }
+    ],
+    "environment": [
+        {
+          "name": "AUTH_URI",
+          "value": "string"
+        },
+        {
+          "name": "TOKEN_URI",
+          "value": "string"
+        },
+        {
+          "name": "AUTH_PROVIDER_CERT_URL",
+          "value": "string"
+        },
+        {
+          "name": "DB_ROOT_USERNAME",
+          "value": "string"
+        },
+        {
+          "name": "DB_ROOT_PASSWORD",
+          "value": "string"
+        },
+        {
+          "name": "DB_HOST",
+          "value": "mongodb"
+        },
+        {
+          "name": "DB_PORT",
+          "value": "27017"
+        },
+        {
+          "name": "MONGODB_URI",
+          "value": "string"
+        },
+        {
+          "name": "DEFAULT_DB",
+          "value": "mongodb"
+        },
+        {
+          "name": "PORT",
+          "value": "5002"
+        },
+        {
+          "name": "AUTH_PRIVATE_KEY_ID",
+          "value": "string"
+        },
+        {
+          "name": "AUTH_PRIVATE_KEY",
+          "value": "string"
+        },
+        {
+          "name": "AUTH_CLIENT_EMAIL",
+          "value": "string"
+        },
+        {
+          "name": "AUTH_CLIENT_ID",
+          "value": "string"
+        },
+        {
+          "name": "AUTH_CLIENT_CERT_URL",
+          "value": "string"
+        },
+        {
+          "name": "AUTH_API_KEY",
+          "value": "string"
+        },
+        {
+          "name": "AUTH_DOMAIN",
+          "value": "string"
+        },
+        {
+          "name": "AUTH_PROJECT_ID",
+          "value": "string"
+        },
+        {
+          "name": "AUTH_STORAGE_BUCKET",
+          "value": "string"
+        },
+        {
+          "name": "AUTH_MESSAGING_SENDER_ID",
+          "value": "string"
+        },
+        {
+          "name": "AUTH_APP_ID",
+          "value": "string"
+        },
+        {
+          "name": "AUTH_MEASUREMENT_ID",
+          "value": "string"
+        },
+        {
+          "name": "PRIVATE_KEY_V2",
+          "value": "string"
+        },
+        {
+          "name": "PRIVATE_KEY_ADMIN_V2",
+          "value": "string"
+        }
     ]
   }
 ]
+
 DEFINITION
   execution_role_arn    = aws_iam_role.ecsTaskExecutionRole.arn
   task_role_arn         = aws_iam_role.ecsTaskRole.arn
